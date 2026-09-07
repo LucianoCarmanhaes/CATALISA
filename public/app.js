68a489ee3b200cf9da3a1206a5ab5e377ad8b755
LucianoCarmanhaes <luciano@botuvera.com>
2026-09-07 14:29:44 -0400

    Corrige recuperação de senha no login

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+document.body.append($('#toast'), $('#error-banner'));
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './app-config.js';
 
